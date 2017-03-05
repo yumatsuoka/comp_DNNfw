653a3be92306ad6d338943603039c5c3c8714ae3
yumatsuoka <a.iebako.yuma@gmail.com>
2017-03-05 16:28:29 +0900

    rename and rm some files

--- run_annie.py
+++
@@ -1,9 +0,0 @@
-import os
-
-model = 'allconvnet'
-gpu = 0
-epoch = 100
-
-for sb in [False, True]:
-    for gn in [1]:
-        os.system("python chainer_cifar.py -e {} -m {} -g {} -gn {} -s {}".format(epoch, model, gpu, gn, sb))
