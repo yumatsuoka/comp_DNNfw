dfc105fbdcd381d68fc5089e48c2209f7d43f05c
Yuma Matsuoka <a.iebako.yuma@gmail.com>
2017-04-19 00:15:22 +0900

    finished to make MXNet codes

--- mxnet_cifar10.py
+++ mxnet_cifar10.py
@@ -1,27 +1,25 @@
 # coding: utf-8
 #!/usr/bin/env python
 
-#from __future__ import print_function
+from __future__ import print_function
 
 import logging
-#logging.basicConfig(level=logging.DEBUG)
 logging.getLogger().setLevel(logging.DEBUG)
 import numpy as np
 import mxnet as mx
 
 import cifar10
-#import cifar100
+import cifar100
+from mxnet_model import resnet, allconvnet, shallownet
 
 
 # ### Hyper parameters
-NUM_GPU = 1
-EPOCH = 100 
+GPUS = [mx.gpu(0)]
+EPOCH = 10 
 BATCH_SIZE = 100
 MODEL_TYPE = "allconvnet"
 DATASET_TYPE = "cifar10"
-GPUS = [mx.gpu(n) for n in range(NUM_GPU)] if NUM_GPU > 1 else\
-        mx.gpu(0) if NUM_GPU==1 else mx.cpu()
-print("epoch:{}, batch:{}, dataset:{}, gpus:{}".format(\
+print("# epoch:{}, batch:{}, dataset:{}, gpus:{}".format(\
         EPOCH, BATCH_SIZE, DATASET_TYPE, GPUS))
 
 
@@ -32,75 +30,23 @@ train_data = dataset['train']['data'].astype(np.float32)/255.
 train_size, image_size, _, num_channels = train_data.shape
 num_labels  = len(list(set(dataset['train']['target'])))
 
-#train_lbl = mx.nd.one_hot(mx.nd.array(dataset['train']['target']), num_labels)
 train_lbl = dataset['train']['target'].astype(np.int8)
 val_data = dataset['test']['data'].astype(np.float32)/255.
-#val_lbl = mx.nd.one_hot(mx.nd.array(dataset['test']['target']), num_labels)
 val_lbl = dataset['test']['target'].astype(np.int8)
 print("# train_size:{}, image_size:{}, num_channels:{}, num_labels:{}".format(
                   train_size, image_size, num_channels, num_labels))
-
 train_iter = mx.io.NDArrayIter(train_data, train_lbl, BATCH_SIZE, shuffle=True)
 val_iter = mx.io.NDArrayIter(val_data, val_lbl, BATCH_SIZE)
 
-####
-# define neural network by debug
-data = mx.symbol.Variable('data')
-#data = mx.sym.Variable('data')
-h1 = mx.sym.Convolution(data=data, pad=(1,1), kernel=(3,3), stride=(1,1), num_filter=32)
-h2 = mx.sym.Activation(data=h1, act_type="relu")
-h3 = mx.sym.Pooling(data=h2, pool_type="max", kernel=(3,3), stride=(2,2))
-#h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192)
-h4 = mx.sym.Convolution(data=h3, pad=(1,1), kernel=(3,3), stride=(1,1), num_filter=64)
-h5 = mx.sym.Activation(data=h4, act_type="relu")
-h6 = mx.sym.Pooling(data=h5, pool_type="max", kernel=(3,3), stride=(2,2))
-h7 = mx.sym.Flatten(data=h6)
-#h8 = mx.sym.FullyConnected(data=h7, num_hidden=1000)
-h9 = mx.sym.Activation(data=h7, act_type="relu")
-h10 = mx.symbol.FullyConnected(data=h9, num_hidden=num_labels)
-#h10 = mx.sym.FullyConnected(data=h7, num_hidden=num_labels)
-#h = mx.sym.Activation(data=h, act_type="relu")
-cnn = mx.sym.SoftmaxOutput(data=h10, name='softmax')
-"""
 
-h = mx.sym.Variable('data')
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(1,1), pad=(1,1))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(1,1), pad=(1,1))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(2,2))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(2,2))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(1, 1), num_filter=192)
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Convolution(data=h, kernel=(1, 1), num_filter=num_labels)
-h = mx.sym.BatchNorm(h)
-h = mx.sym.Activation(data=h, act_type="relu")
-h = mx.sym.Pooling(data=h, global_pool=True, pool_type="avg", kernel=(8, 8))
-h = mx.sym.Flatten(data=h)
-cnn = mx.sym.SoftmaxOutput(data=h, name='softmax')
-#return cnn
-"""
+# ### define net and trainer
+net = allconvnet if MODEL_TYPE=='allconvnet' else\
+        resnet if MODEL_TYPE=='resnet' else shallownet
 
-#### 
+#### Load trainer
 model = mx.mod.Module(
         context=GPUS,
-        symbol = cnn,
+        symbol = net(num_labels),
         )
 
 model.fit(
@@ -109,11 +55,9 @@ model.fit(
         num_epoch = EPOCH,
         optimizer_params={'learning_rate':0.001},
         optimizer='adam',
-        #eval_metric=['acc', 'ce'],
         batch_end_callback = mx.callback.Speedometer(BATCH_SIZE, 500)
         )
 
 
 prob = model.predict(val_iter)[0].asnumpy()
 print('Classified as %d with probability %f' % (prob.argmax(), max(prob)))
-
--- mxnet_model.py
+++ mxnet_model.py
@@ -1,23 +1,105 @@
 # coding: utf-8
-#!/usr/bin/env python
 
-from __future__ import print_function
-
-import logging
-logging.basicConfig(level=logging.DEBUG)
-#logging.getLogger().setLevel(logging.DEBUG)
-import numpy as np
 import mxnet as mx
 
-import cifar10
-import cifar100
-from mxnet_model import AllConvNetBN, ResNet
+
+### shallow cnn
+def shallownet(num_labels=10):
+    h = mx.symbol.Variable('data')
+    h = mx.sym.Convolution(data=h, pad=(1,1), kernel=(3,3), stride=(1,1), num_filter=32)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Pooling(data=h, pool_type="max", kernel=(3,3), stride=(2,2))
+    h = mx.sym.Convolution(data=h, pad=(1,1), kernel=(3,3), stride=(1,1), num_filter=64)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Pooling(data=h, pool_type="max", kernel=(3,3), stride=(2,2))
+    h = mx.sym.Flatten(data=h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.symbol.FullyConnected(data=h, num_hidden=num_labels)
+    loss = mx.sym.SoftmaxOutput(data=h, name='softmax')
+    return loss 
 
 
-def allConvNetBN():
-    data = mx.sym.Variable('data')
-    conv1 = mx.sym.Convolution(data=data, kernel=(3, 3), num_filter=32)
-    relu1 = mx.sym.Activation(data=conv1, act_type="relu")
-    fc1 = mx.sym.FullyConnected(data=data, name='fc1', num_hidden=128)
-    act1 = mx.sym.Activation(data=fc1, name='relu1', act_type="relu")
-    mlp = mx.sym.SoftmaxOutput(data=fc3, name='softmax')
+### AllConvolutionalNet
+def allconvnet(num_labels=10):
+    h = mx.sym.Variable('data')
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(1,1), pad=(1,1))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(1,1), pad=(1,1))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=96, stride=(2,2))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(2,2))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(3, 3), num_filter=192, stride=(1,1), pad=(1,1))
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(1, 1), num_filter=192)
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Convolution(data=h, kernel=(1, 1), num_filter=num_labels)
+    h = mx.sym.BatchNorm(h)
+    h = mx.sym.Activation(data=h, act_type="relu")
+    h = mx.sym.Pooling(data=h, global_pool=True, pool_type="avg", kernel=(8, 8))
+    h = mx.sym.Flatten(data=h)
+    loss = mx.sym.SoftmaxOutput(data=h, name='softmax')
+    return loss
+
+
+### ResNet
+def resnet_block_fast(input_data, num_filter, name):
+    conv1 = mx.symbol.Convolution(data=input_data, 
+                                     kernel=(3, 3), 
+                                     stride=(2, 2), 
+                                     pad=(0, 0), 
+                                     num_filter=num_filter, 
+                                     name='conv'+name)
+    conv1 = mx.symbol.Activation(data=conv1, act_type='relu', name='relu'+name)
+    conv2 = mx.symbol.Convolution(data=conv1, 
+                                     kernel=(3, 3), 
+                                     stride=(1, 1), 
+                                     pad=(1, 1), 
+                                     num_filter=num_filter, 
+                                     name='conv'+name+'_1')
+    conv2 = mx.symbol.Activation(data=conv2, act_type='relu', name='relu'+name+'_1')
+    conv3 = mx.symbol.Convolution(data=conv2, 
+                                     kernel=(3, 3), 
+                                     stride=(1, 1), 
+                                     pad=(1, 1), 
+                                     num_filter=num_filter, 
+                                     name='conv'+name+'_2')
+    conv3 = mx.symbol.Activation(data=conv3, act_type='relu', name='relu'+name+'_2')
+    return conv3 + conv1
+
+def resnet(num_labels=10):
+    """
+    LeCun, Yann, Leon Bottou, Yoshua Bengio, and Patrick
+    Haffner. "Gradient-based learning applied to document recognition."
+    Proceedings of the IEEE (1998)
+    This is from https://github.com/youansheng/fast_center_loss/blob/master/train.py
+    """
+    data = mx.symbol.Variable('data')
+
+    conv1 = mx.symbol.Convolution(data=data, kernel=(3, 3), 
+                                     stride=(2, 2), pad=(0, 0), 
+                                     num_filter=32, name='conv1')
+    conv1 = mx.symbol.Activation(data=conv1, act_type='relu', name='relu1')
+    res2 = resnet_block_fast(conv1, 32, '2')
+    res3 = resnet_block_fast(res2, 32, '3')
+    res4 = resnet_block_fast(res3, 64, '4')
+    res5 = resnet_block_fast(res4, 128, '5')
+    flatten = mx.symbol.Flatten(data=res5)
+    fc6 = mx.symbol.FullyConnected(data=flatten, num_hidden=256, name='fc6')
+    fc7 = mx.symbol.FullyConnected(data=fc6, num_hidden=num_labels, name='fc7')
+
+    loss = mx.symbol.SoftmaxOutput(data=fc7, name='softmax')
+    return loss
